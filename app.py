e5206334d0287f58fc98580b37f4953c24dff2c4
kashish0603 <kashish.jain2021a@vitstudent.ac.in>
2022-08-06 23:23:22 +0530

    MySQL Connection

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from flask import Flask,render_template,request,redirect,session,url_for
 import re
+from flask_mysqldb import MySQL
 # from flask_sqlalchemy import SQLAlchemy
+import MySQLdb.cursors
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:VIT_Vellore21@localhost/pyxis'
+app.secret_key = 'your secret key'
 
-# def check_username(username,password):
-#     try:
-#         mail = db.child("Usernames").child(username).get().val()
-#         auth.sign_in_with_email_and_password(mail, password)
-#         return True
-#     except:
-#         return False
-
-
-
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'VIT_Vellore21'
+app.config['MYSQL_DB'] = 'Pyxis_team'
+  
+  
+mysql = MySQL(app)
+  
+  
 @app.route('/')
 @app.route('/login', methods =['GET', 'POST'])
 def login():
@@ -23,44 +24,61 @@ def login():
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
-        if check_username(username,password):
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM User WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
             session['loggedin'] = True
-            session['username'] = username
+            session['id'] = account['id']
+            session['username'] = account['username']
             msg = 'Logged in successfully !'
-            return render_template('index.html', msg = msg, username=username)
+            return render_template('index.html', msg = msg)
         else:
             msg = 'Incorrect username / password !'
     return render_template('login.html', msg = msg)
-  
-@app.route('/logout')
-def logout():
-    session.pop('loggedin', None)
-    session.pop('username', None)
-    return redirect(url_for('login'))
-  
+
 @app.route('/register', methods =['GET', 'POST'])
 def register():
     msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
         username = request.form['username']
         password = request.form['password']
-        email = request.form['email']
-        if check_username(username,password):
+        email = request.form['email'] 
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM User WHERE username = % s', (username, ))
+        account = cursor.fetchone()
+        if account:
             msg = 'Account already exists !'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             msg = 'Invalid email address !'
         elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers !'
-        elif not username or not password or not email:
-            msg = 'Please fill out the form !'
+            msg = 'name must contain only characters and numbers !'
         else:
-            auth.create_user_with_email_and_password(email,password)
-            db.child("Usernames").update({username:email})
+            cursor.execute('INSERT INTO User VALUES (NULL, % s, % s, % s)', (username, password, email ))
+            mysql.connection.commit()
             msg = 'You have successfully registered !'
     elif request.method == 'POST':
         msg = 'Please fill out the form !'
     return render_template('register.html', msg = msg)
 
+@app.route("/index")
+def index():
+    if 'loggedin' in session:
+        return render_template('index.html')
+    return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   return redirect(url_for('login'))
+
+@app.route("/aadhar")
+def aadhar():
+    if 'loggedin' in session:
+        return render_template('aadhar.html')
+    
 
 if __name__ == '__main__':
-    app.run(debug=True)    
+    app.run(debug=True, host ="localhost", port = int("5000"))    
