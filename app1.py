09713b6bc6170ba913a3afbc052b0d8a68eb860a
unknown <virajit.vinaydutt2021@vitstudent.ac.in>
2022-08-24 18:18:57 +0530

    Integrated the Back-End and modified the files

--- app1.py
+++ app1.py
@@ -1,8 +1,10 @@
+from email import message
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask import session as login_session
 from flask import make_response
 import os
-from face_verification.new import facerecog
+from face_verification.new import facerecog, face_match
+from Biometric.biometric import biometrics
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -14,7 +16,6 @@ def index():
             return redirect(url_for('select'))
     return render_template('login.html')
 
-
 @app.route('/select', methods=['GET', 'POST'])
 def select():
     if request.method == 'POST':
@@ -24,42 +25,61 @@ def select():
 @app.route('/biometric', methods=['GET', 'POST'])
 def biometric():
     if request.method == 'POST':
-        return redirect(url_for('index'))
+        img = request.files['image']
+        img.save(os.path.join('static/frames', img.filename))
+
+        # If img.jpg already exists, delete it
+        if os.path.exists('static/frames/img.tif'):
+            os.remove('static/frames/img.tif')
+        # Rename the image to image.jpg
+        os.rename(os.path.join('static/frames', img.filename), os.path.join('static/frames', 'img.tif'))
+
+        return redirect(url_for('searching',messages=0))
     return render_template('biometric.html')
 
 @app.route('/cctv', methods=['GET', 'POST'])
 def cctv():
     if request.method == 'POST':
         img = request.files['image']
-        img.save(os.path.join('static', img.filename))
+        img.save(os.path.join('static/frames', img.filename))
 
         # If img.jpg already exists, delete it
-        if os.path.exists('static/img.jpg'):
-            os.remove('static/img.jpg')
+        if os.path.exists('static/frames/img.jpg'):
+            os.remove('static/frames/img.jpg')
         # Rename the image to image.jpg
-        os.rename(os.path.join('static', img.filename), os.path.join('static', 'img.jpg'))
+        os.rename(os.path.join('static/frames', img.filename), os.path.join('static/frames', 'img.jpg'))
         
-        return redirect(url_for('searching'))
+        return redirect(url_for('searching',messages=1))
 
-
-
-
-
-
-        return redirect(url_for('index'))
     return render_template('cctv.html')
 
 @app.route('/searching', methods=['GET', 'POST'])
 def searching():
-    if request.method == 'POST':
-        if request.form['realtime']:
-            facerecog()
-    return render_template('searching.html')
+    mess = request.args['messages']
+    print(mess)
+    if int(mess)==1:
+        result = face_match('static/frames/img.jpg','face_verification/data2.pt')
+        folder = os.path.join('static/photos',result[0])
+        print("Photo matched with: ",result[0])
+        file = os.path.join(folder,os.listdir(os.path.join('static/photos',result[0]))[0])
+    elif int(mess)==2:
+        result = facerecog()
+        folder = os.path.join('static/photos',result[0])
+        print("Face matched with: ",result[0])
+        file = os.path.join(folder,os.listdir(os.path.join('static/photos',result[0]))[0])
+    else:
+        result = biometrics('static/frames/img.tif')
+        folder = os.path.join('static/photos',result)
+        os.remove('static/frames/img.tif')
+        print("Fingerprint matched with: ",result)
+        file = os.path.join(folder,os.listdir(os.path.join('static/photos',result))[0])
+
+    return render_template('searching.html',img_path=file)
 
 @app.route('/aadhar', methods=['GET', 'POST'])
 def aadhar():
     if request.method == 'POST':
-        return redirect(url_for('index'))
+        return redirect(url_for('searching',messages=2))
     return render_template('aadhar.html')
 
 
--- face_verification/new.py
+++ face_verification/new.py
@@ -1,17 +1,15 @@
-<<<<<<< HEAD
 from facenet_pytorch import MTCNN, InceptionResnetV1
 import torch
 from torchvision import datasets
 from torch.utils.data import DataLoader
 from PIL import Image
 import os
-=======
->>>>>>> 2d5ade31f65880a6688555c4dc01b04853f1d186
+import cv2
+import face_recognition
+import numpy as np
+
 def facerecog():
     # import libraries
-    import cv2
-    import face_recognition
-    import numpy as np
 
     # Get a reference to webcam 
     video_capture = cv2.VideoCapture(0)
@@ -38,48 +36,30 @@ def facerecog():
 
         count+=1
 
-        if face_locations!=[] and count%10==0:
-            img_path = 'images/frame'+str(count+1)+".jpg"
+        if face_locations!=[] and count%5==0:
+            img_path = 'static/frames/frame'+str(count+1)+".jpg"
             cv2.imwrite(img_path,frame)
 
-<<<<<<< HEAD
-            result = face_match(img_path,'pytorch_face_recognition//data2.pt')
-=======
-            result = face_match(img_path,'FaceRec2//pytorch_face_recognition//data2.pt')
->>>>>>> 2d5ade31f65880a6688555c4dc01b04853f1d186
+
+            result = face_match(img_path,'face_verification/data2.pt')
 
             if result[1]>1.0:
                 print("No match")
             else:
-                print('Face matched with: ',result[0], 'With distance: ',result[1])
+                return result
+                break
                 
         
         cv2.imshow("Footage",frame)
         
         if cv2.waitKey(1) & 0XFF == ord('q'):
             break
-        # Display the resulting image
-        # if writer is None:
-        #     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        #     writer = cv2.VideoWriter('processed.avi', fourcc, 30, (frame.shape[1], frame.shape[0]), True)
         
-        print(count)
 
     # writer.release()
     video_capture.release()
 
-def face_match(img_path, data_path): # img_path= location of photo, data_path= location of data.pt 
-<<<<<<< HEAD
-
-=======
-    from facenet_pytorch import MTCNN, InceptionResnetV1
-    import torch
-    from torchvision import datasets
-    from torch.utils.data import DataLoader
-    from PIL import Image
-    import os
->>>>>>> 2d5ade31f65880a6688555c4dc01b04853f1d186
-
+def face_match(img_path, data_path, img_vid=False): # img_path= location of photo, data_path= location of data.pt 
     mtcnn = MTCNN(image_size=240, margin=0, min_face_size=20)
     resnet = InceptionResnetV1(pretrained='vggface2').eval()
     
@@ -98,15 +78,14 @@ def face_match(img_path, data_path): # img_path= location of photo, data_path= l
         dist_list.append(dist)
         
     idx_min = dist_list.index(min(dist_list))
-    # return (name_list[idx_min], min(dist_list))
-    result = (name_list[-1], dist_list[-5])
+    result = (name_list[idx_min], min(dist_list))
 
     os.remove(img_path)
 
-    return result
-<<<<<<< HEAD
-=======
+    if result[1]>1:
+        return "No result"
+    else:
+        return result
 
 if __name__ == '__main__':
     facerecog()
->>>>>>> 2d5ade31f65880a6688555c4dc01b04853f1d186
